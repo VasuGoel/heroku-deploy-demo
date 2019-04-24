f23eec5179fd665a6f85367f76c69255d2a48336
Vasu Goel <goelvasu0712@gmail.com>
2019-04-24 09:25:46 +0000

    Change route route, add image-gallery route

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ app.use(express.static('public'));
 
 // routes
 app.get('/', (req, res) => {
-    res.redirect("/portfolio");
+    res.redirect("/image-gallery");
 });
 
 app.get('/portfolio', (req, res) => {
     res.render('portfolio');
 });
 
+app.get('/image-gallery', (req, res) => {
+    res.render('image-gallery'); 
+});
 
 // start UNIX socket to listen for connections
 app.listen(process.env.PORT, process.env.IP, () => {
